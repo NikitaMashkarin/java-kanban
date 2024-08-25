0dd7eb236c59067eaf1e175d2ee569dd7d2e6329
Nikita Mashkarin <mashkarinnikita@yandex.ru>
2024-08-25 14:25:52 +0300

    Удалить все задачи

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -7,39 +7,23 @@ public class TaskManager {
         HashMap<Integer, Epic> epics = new HashMap<>();
         HashMap<Integer, Subtask> subtasks = new HashMap<>();
 
+
     }
 
-    public static void printAllTasks (HashMap<Integer, Task> tasks) {
-        if (tasks.isEmpty()) {
-            System.out.println("Список задач пуст");
-            return;
-        }
+    public static void printAllTasks (HashMap<Integer, Task> tasks, HashMap<Integer, Epic> epics,
+                                      HashMap<Integer, Subtask> subtasks) {
+        System.out.println("Все задачи:");
 
         for (Task task : tasks.values()) {
             if (task != null) {
                 System.out.println(task.toString());
             }
         }
-    }
-
-
-    public void getAllEpics (HashMap<Integer, Epic> epics) {
-        if (epics.isEmpty()) {
-            System.out.println("Список задач пуст");
-            return;
-        }
 
         for (Task epic : epics.values()) {
             if (epic != null) {
                 System.out.println(epic.toString());
             }
-        }
-    }
-
-    public void getAllSubtasks (HashMap<Integer, Subtask> subtasks) {
-        if (subtasks.isEmpty()) {
-            System.out.println("Список задач пуст");
-            return;
         }
 
         for (Task subtask : subtasks.values()) {
@@ -48,4 +32,13 @@ public class TaskManager {
             }
         }
     }
+
+    public static void removeAllTasks (HashMap<Integer, Task> tasks, HashMap<Integer, Epic> epics,
+                    HashMap<Integer, Subtask> subtasks) {
+        tasks.clear();
+        epics.clear();
+        subtasks.clear();
+    }
+
+
 }
