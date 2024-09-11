f32252531382a9d334d8b65f87b68889ee90b355
Nikita Mashkarin <mashkarinnikita@yandex.ru>
2024-09-12 01:18:52 +0300

    Написаны тесты для Managers

--- src/com/yandex/taskTracker/test/service/ManagersTest.java
+++ src/com/yandex/taskTracker/test/service/ManagersTest.java
@@ -1,7 +1,18 @@
 package com.yandex.taskTracker.test.service;
 
+import com.yandex.taskTracker.service.*;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagersTest {
+    @Test
+    public void f() {
+        assertInstanceOf(InMemoryTaskManager.class, Managers.getDefault());
+    }
 
+    @Test
+    public void g() {
+        assertInstanceOf(InMemoryHistoryManager.class, Managers.getDefaultHistory());
+    }
 }
