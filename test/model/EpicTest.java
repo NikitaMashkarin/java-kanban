d10353d9a003d54ebf6563c15d49341c12e71b44
Nikita Mashkarin <mashkarinnikita@yandex.ru>
2024-09-12 15:39:41 +0300

    Добавленны тесты для model

--- test/model/EpicTest.java
+++ test/model/EpicTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class EpicTest {
     @Test
     public void epicsWithEqualIdShouldBeEqual() {
-        Epic epic1 = new Epic("Name", "Description", 1, StatusTask.NEW);
+        Epic epic1 = new Epic("Name2", "Description2", 1, StatusTask.NEW);
         Epic epic2 = new Epic("Name1", "Description1", 1, StatusTask.NEW);
         Assertions.assertEquals(epic1, epic2, "Епки должны быть равны, если равны их id");
     }
