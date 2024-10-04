43dcc2d89b5748dff4b394947f6ad3151f276dad
Nikita Mashkarin <mashkarinnikita@yandex.ru>
2024-10-04 19:23:43 +0300

    Исапрвить логику InMemoryHistoryManager

--- src/com/yandex/taskTracker/service/InMemoryHistoryManager.java
+++ src/com/yandex/taskTracker/service/InMemoryHistoryManager.java
@@ -2,25 +2,81 @@ package com.yandex.taskTracker.service;
 
 import com.yandex.taskTracker.model.Task;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    private static final int MAX_SIZE_HISTORY = 10;
-    private final List<Task> history = new LinkedList<>();
+
+    private static class Node {
+        Task item;
+        Node next;
+        Node prev;
+
+        Node(Node prev, Task element, Node next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private final Map<Integer, Node> history = new HashMap<>();
+
+    private void linkLast(Task task){
+        final Node oldTail = tail;
+        final Node newNode = new Node(tail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        }
+        else {
+            oldTail.next = newNode;
+        }
+        history.put(task.getId(), newNode);
+    }
+
+    private void removeNode(Node node){
+        if(node == null){
+            return;
+        }
+
+        if(node.prev == null){
+            head = node.next;
+            node.next.prev = null;
+        } else {
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+        }
+
+        if (node.next == null){
+            tail = node.prev;
+            node.prev.next = null;
+        } else {
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+        }
+    }
+    @Override
+    public void add(Task task){
+        if(history.containsKey(task.getId())){
+            removeNode(history.get(task.getId()));
+        }
+        linkLast(task);
+    }
 
     @Override
-    public void add(Task task) {
-        if (history.size() >= MAX_SIZE_HISTORY){
-            history.remove(0);
-        }
-        history.add(task);
+    public void remove(int id){
+        history.remove(id);
+
     }
 
     @Override
     public List<Task> getHistory(){
-        return new LinkedList<>(history);
+        List<Task> tasks = new ArrayList<>();
+        Node current = head;
+        while (current != null){
+            tasks.add(current.item);
+            current = current.next;
+        }
+        return tasks;
     }
-
 }
